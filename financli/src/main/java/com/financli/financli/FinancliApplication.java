452cd5848b2b3f026aa344cc6fc09e177b87ad0c
CarlosEduardoBorges <eduardoborges705@hotmail.com>
2026-04-07 13:11:21 -0300

    Primeiro commit do projeto

---
+++ financli/src/main/java/com/financli/financli/FinancliApplication.java
@@ -0,0 +1,13 @@
+package com.financli.financli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancliApplication.class, args);
+	}
+
+}
---
+++ financli/src/test/java/com/financli/financli/FinancliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financli.financli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
