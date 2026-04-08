5963dbf7dc79de173cb6b35a8c4f3be1a12ce6ac
CarlosEduardoBorges <eduardoborges705@hotmail.com>
2026-04-08 11:37:25 -0300

    Configurada a aplicação Spring Boot para executar como CLI (sem servidor web)

--- financli/src/main/java/com/financli/FinancliApplication.java
+++ financli/src/main/java/com/financli/FinancliApplication.java
@@ -1,13 +1,15 @@
 package com.financli;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinancliApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinancliApplication.class, args);
+		SpringApplication app = new SpringApplication(FinancliApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
-
 }
