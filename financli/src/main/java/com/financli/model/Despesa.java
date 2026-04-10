9fe4000f0a825ff13ce3bced879832438853997f
CarlosEduardoBorges <eduardoborges705@hotmail.com>
2026-04-10 11:16:25 -0300

    corrige violações do Checkstyle removendo import com wildcard (utilizando imports explícitos)

--- financli/src/main/java/com/financli/model/Despesa.java
+++ financli/src/main/java/com/financli/model/Despesa.java
@@ -1,6 +1,12 @@
 package com.financli.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
