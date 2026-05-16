c3d3a42223b310233bf8eec39c1bce01c1498c37
CarlosEduardoBorges <eduardoborges705@hotmail.com>
2026-05-16 18:57:18 -0300

    test(CotacaoServiceIntegrationTest): substituir teste real por mock com wiremock

--- src/test/java/com/financli/service/CotacaoServiceIntegrationTest.java
+++ src/test/java/com/financli/service/CotacaoServiceIntegrationTest.java
@@ -1,6 +1,11 @@
 package com.financli.service;
 
 import com.financli.model.CotacaoResponse;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,15 +13,42 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(properties = {"cotacao.api.url=http://localhost:9876/json/last/USD-BRL,EUR-BRL"})
 @ActiveProfiles("test")
 class CotacaoServiceIntegrationTest {
+
+    static WireMockServer wireMock = new WireMockServer(
+            WireMockConfiguration.wireMockConfig().port(9876));
+
+    @BeforeAll
+    static void iniciarWireMock() {
+        wireMock.start();
+    }
+
+    @AfterAll
+    static void encerrarWireMock() {
+        wireMock.stop();
+    }
 
     @Autowired
     private CotacaoService cotacaoService;
 
     @Test
     void deveBuscarCotacoesComSucesso() {
+        wireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/json/last/USD-BRL,EUR-BRL"))
+                .willReturn(WireMock.aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("""
+                                {
+                                    "USDBRL": {"bid": "5.75", "ask": "5.76",
+                                               "name": "Dólar Americano/Real Brasileiro"},
+                                    "EURBRL": {"bid": "6.20", "ask": "6.21",
+                                               "name": "Euro/Real Brasileiro"}
+                                }
+                                """)
+                )
+        );
+
         CotacaoResponse resposta = cotacaoService.buscarCotacoes();
 
         assertThat(resposta).isNotNull();
