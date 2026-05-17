aedf9163fffba677e12bdaea777e2a08422d9225
CarlosEduardoBorges <eduardoborges705@hotmail.com>
2026-05-16 23:32:46 -0300

    refactor(FinancliApplication): remover WebApplicationType.NONE redundante

--- src/main/java/com/financli/FinancliApplication.java
+++ src/main/java/com/financli/FinancliApplication.java
@@ -1,15 +1,12 @@
 package com.financli;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinancliApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(FinancliApplication.class);
-		app.setWebApplicationType(WebApplicationType.NONE);
-		app.run(args);
+		SpringApplication.run(FinancliApplication.class, args);
 	}
 }
